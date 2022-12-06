883e984b9f7e25dfb541757e0b55b471f18fab06
Lewis-zzZ <lewis0721@outlook.com>
2022-12-06 14:26:53 +0000

    Basic Function update
    - Update lisp.h & testlisp.c from Neil
    - Finish Basic Function (First Version)

--- Code/Week11/CarCdr/Linked/linked.c
+++ Code/Week11/CarCdr/Linked/linked.c
@@ -1,5 +1,10 @@
 #include "../lisp.h"
 #include "specific.h"
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXLENGTH 1000
+#define RADIX 10
 
 lisp* lisp_atom(const atomtype a){
     lisp* new_lisp = (lisp*)ncalloc(1, sizeof(lisp*));
@@ -32,20 +37,53 @@ atomtype lisp_getval(const lisp* l){
     return (l->val);
 }
 
+bool lisp_isatomic(const lisp* l){
+    if (l->car == NULL && l->cdr == NULL){
+        return 1;
+    }
+    return 0;
+}
+
 lisp* lisp_copy(const lisp* l){
     lisp* new_lisp = (lisp*)ncalloc(1, sizeof(lisp*));
+
     memcpy(new_lisp->car, l->car, sizeof(lisp));
     memcpy(new_lisp->cdr, l->cdr, sizeof(lisp));
     atomtype temp_val = l->val;
-    memcpy(new_lisp->val, temp_val, sizeof(atomtype));
+    memcpy(&new_lisp->val, &temp_val, sizeof(atomtype));
     return new_lisp;
 }
 
-int lisp_length(const lisp* l){
+void lisp_count_cdr(const lisp* l, int* len){
+    if (lisp_cdr(l) != NULL){
+        ++(*len);
+        lisp_count_cdr(lisp_cdr(l), len);
+    }
+}
 
+int lisp_length(const lisp* l){
+    int len = 0;
+    lisp_count_cdr(l, &len);
+    return len;
 }
 
 void lisp_tostring(const lisp* l, char* str){
+    if (lisp_isatomic(l)){
+        char temp[MAXLENGTH];
+        itoa(lisp_getval(l), temp, RADIX);
+        strcat(str, temp);
+        if (lisp_cdr(l) == NULL){
+            strcat(str, ")");
+        } else{
+            strcat(str, " ");
+        }
+        lisp_tostring(lisp_car(l), str);
+        lisp_tostring(lisp_cdr(l), str);
+    } else{
+        strcat("(");
+        lisp_tostring(lisp_car(l), str);
+        lisp_tostring(lisp_cdr(l), str);
+    }
 
 }
 
@@ -60,3 +98,6 @@ void lisp_free(lisp** l){
     *l = NULL;
 }
 
+void test(void){
+
+}
